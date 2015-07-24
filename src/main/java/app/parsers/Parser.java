5d8aed5b8d128b3836c95a36785eeb0acc5de459
Jonathan Peck <peckjonathan@gmail.com>
2015-07-24 23:07:57 +0200

    Rewrote parsers

--- src/main/java/app/parsers/Parser.java
+++ src/main/java/app/parsers/Parser.java
@@ -1,11 +1,43 @@
 package app.parsers;
 
+import java.util.Collection;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+
 /**
- * Interface for JSON parsers.
- * @param <T> Type of object to parse
+ * Interface for parsers.
+ * @param <T> Type to parse
  * @author jonathan
  */
-public interface Parser<T> {
-    String toJSON(T object);
-    T toObject(String json);
+public abstract class Parser<T> {
+    /**
+     * Convert object to JSON string
+     * @param object Object to be converted
+     * @return JSON string
+     */
+    public abstract String toJson(T object);
+    
+    /**
+     * Convert JSON string into object
+     * @param json JSON string
+     * @return Object
+     */
+    public abstract T fromJson(String json);
+    
+    /**
+     * Convert a collection of objects to JSON
+     * @param objects Collection to convert
+     * @return JSON encoding of collection
+     */
+    public String toJson(Collection<T> objects) {
+        JsonParser parser = new JsonParser();
+        JsonArray results = new JsonArray();
+        for(T object: objects) {
+            JsonElement element = parser.parse(toJson(object));
+            results.add(element);
+        }
+        
+        return results.getAsString();
+    }
 }
---
+++ src/main/java/app/parsers/UserParser.java
@@ -0,0 +1,21 @@
+package app.parsers;
+
+import app.domain.users.User;
+
+/**
+ * Parser for user objects.
+ * @author jonathan
+ */
+public class UserParser extends Parser<User> {
+
+    @Override
+    public String toJson(User object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public User fromJson(String json) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
