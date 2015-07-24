709bf019bd93fbd926ecc3170348f1d79153e4fa
Jonathan Peck <peckjonathan@gmail.com>
2015-07-24 21:16:49 +0200

    Config changes

---
+++ src/main/java/app/spring/SpringMain.java
@@ -0,0 +1,14 @@
+package app.spring;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Spring main class.
+ * @author jonathan
+ */
+@Controller
+@EnableAutoConfiguration
+public class SpringMain {
+    
+}
