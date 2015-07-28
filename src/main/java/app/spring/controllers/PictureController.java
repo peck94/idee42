570e19fbb241392b83196cc741424c403d1db706
Jonathan Peck <peckjonathan@gmail.com>
2015-07-28 17:09:31 +0200

    Pictures listing and GUI tweaks

--- src/main/java/app/spring/controllers/PictureController.java
+++ src/main/java/app/spring/controllers/PictureController.java
@@ -2,20 +2,25 @@ package app.spring.controllers;
 
 import app.controllers.PictureManager;
 import app.controllers.SessionManager;
+import app.domain.pictures.Picture;
 import app.domain.users.SessionKey;
 import app.domain.users.User;
 import app.exceptions.ControllerException;
 import app.exceptions.SpringException;
+import app.parsers.PictureParser;
 import java.math.BigInteger;
+import java.text.ParseException;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Handles liking and disliking of pictures.
+ * Handles operations on pictures.
  * The following constraints are enforced:
  * <ul>
  * <li>only logged-in users can rate pictures</li>
@@ -25,13 +30,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @author jonathan
  */
 @RestController
-public class RateController {
+public class PictureController {
     // store picture manager
     @Autowired
     private PictureManager pictureManager;
     // store session manager
     @Autowired
     private SessionManager sessionManager;
+    // parser
+    private PictureParser parser;
+    
+    public PictureController() {
+        parser = new PictureParser();
+    }
     
     /**
      * Send a like to a picture
@@ -83,6 +94,28 @@ public class RateController {
             User user = sessionManager.getUser(key);
             pictureManager.dislikePicture(user, pic);
         }catch(ControllerException e) {
+            throw new SpringException(e);
+        }
+    }
+    
+    /**
+     * Get a listing of all pictures owned by a certain user.
+     * @param auth Session key
+     * @throws app.exceptions.SpringException
+     */
+    @RequestMapping(value="/api/pictures", method=GET)
+    public String list(
+        @RequestHeader(value="auth") String auth) throws SpringException {
+        SessionKey key = new SessionKey(auth);
+        if(!sessionManager.isLoggedIn(key)) {
+            throw new SpringException("Invalid session");
+        }
+        
+        try{
+            User user = sessionManager.getUser(key);
+            List<Picture> pics = pictureManager.getPictures(user.getUsername()).getTarget();
+            return parser.toJsonArray(pics);
+        }catch(ControllerException | ParseException e) {
             throw new SpringException(e);
         }
     }
