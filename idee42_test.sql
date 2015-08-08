6692caa4b241e2d549bc1d84fa411ca3c7a320c7
Jonathan Peck <peckjonathan@gmail.com>
2015-08-08 18:13:14 +0200

    DB schemas

---
+++ idee42_test.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 4.3.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Gegenereerd op: 08 aug 2015 om 16:12
+-- Serverversie: 5.6.23
+-- PHP-versie: 5.5.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `idee42_test`
+--
+CREATE DATABASE IF NOT EXISTS `idee42_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `idee42_test`;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `pictures`
+--
+
+DROP TABLE IF EXISTS `pictures`;
+CREATE TABLE IF NOT EXISTS `pictures` (
+  `id` bigint(20) NOT NULL,
+  `image` text NOT NULL,
+  `date` text NOT NULL,
+  `likes` bigint(20) NOT NULL,
+  `dislikes` bigint(20) NOT NULL,
+  `owner` bigint(20) NOT NULL,
+  `actors` text NOT NULL,
+  `expired` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `email` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `pictures`
+--
+ALTER TABLE `pictures`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `pictures`
+--
+ALTER TABLE `pictures`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT voor een tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
